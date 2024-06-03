eeb0c703eb9dba97d7d2c05c275db8adcbdaae65
CapstoneProject <riqialamsyah5@gmail.com>
2024-06-03 14:28:40 +0700

    Riqi

---
+++ app/src/main/java/com/vetion/capstoneproject/ButtonActivity.kt
@@ -0,0 +1,35 @@
+package com.vetion.capstoneproject
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.vetion.capstoneproject.databinding.ActivityButtonBinding
+
+class ButtonActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityButtonBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityButtonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_button)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
